fb69cf0528efe37e0c198116b2041c4df1fc1abe
LEtMeDEv <79841@naver.com>
2023-12-06 05:04:23 +0900

    add optionSelector

---
+++ src/components/optionSelector/optionSelector.tsx
@@ -0,0 +1,9 @@
+export const OptionSelector = () => {
+  return (
+    <select multiple>
+      <option value="1">A</option>
+      <option value="2">B</option>
+      <option value="3">C</option>
+    </select>
+  );
+};
