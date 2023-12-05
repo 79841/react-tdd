331a0de4edbc74b100af8f821f20f4626460f91a
LEtMeDEv <79841@naver.com>
2023-12-05 19:04:25 +0900

    add skills test

---
+++ src/components/skills/skills.test.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable testing-library/no-debugging-utils */
+import { render, screen } from "@testing-library/react";
+import { Skills } from "./skills";
+
+describe("Skills", () => {
+  const skills = ["HTML", "CSS", "JavaScript"];
+
+  test("renders correctly", () => {
+    render(<Skills skills={skills} />);
+    const listElement = screen.getByRole("list");
+    expect(listElement).toBeInTheDocument();
+  });
+
+  test("renders a list of skills", () => {
+    render(<Skills skills={skills} />);
+    const listItemElements = screen.getAllByRole("listitem");
+    expect(listItemElements).toHaveLength(skills.length);
+  });
+
+  test("renders Login button", () => {
+    render(<Skills skills={skills} />);
+    const loginButton = screen.getByRole("button", {
+      name: "Login",
+    });
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  test("Start Learning button is not rendered", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start Learning",
+    });
+    expect(startLearningButton).not.toBeInTheDocument();
+  });
+
+  test("Start Learning button is eventually displayed", async () => {
+    render(<Skills skills={skills} />);
+    screen.debug();
+    const startLearningButton = await screen.findByRole(
+      "button",
+      {
+        name: "Start Learning",
+      },
+      {
+        timeout: 2000,
+      }
+    );
+    screen.debug();
+    expect(startLearningButton).toBeInTheDocument();
+  });
+});
