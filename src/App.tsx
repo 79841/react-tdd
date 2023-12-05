6072da627ca5b949e1f2556021412bd3aeeec3be
LEtMeDEv <79841@naver.com>
2023-12-06 05:02:12 +0900

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Application } from "./components/application/application";
-import { Skills } from "./components/skills/skills";
+import { MuiMode } from "./components/mui/mui-mode";
+import { AppProviders } from "./providers/app-provider";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Application /> */}
-      <Skills skills={["html"]} />
-    </div>
+    <AppProviders>
+      <div className="App">
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
