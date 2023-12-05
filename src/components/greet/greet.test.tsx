4058b524b2cea7edc02ca6b7032648b7ce21c256
LEtMeDEv <79841@naver.com>
2023-12-05 19:03:49 +0900

    update greet test

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -9,15 +9,7 @@ import { Greet } from "./greet";
 describe("Greet", () => {
   test("Greet render correctly", () => {
     render(<Greet />);
-    const textElement = screen.getByText("Hello");
-    expect(textElement).toBeInTheDocument();
-  });
-});
-
-describe("Nested", () => {
-  test("Greet renders with a name", () => {
-    render(<Greet name="Vishwas" />);
-    const textElement = screen.getByText("Hello Vishwas");
+    const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
   });
 });
